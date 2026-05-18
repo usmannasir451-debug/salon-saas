6f6e349cae1b6a20b6b72095a7b514d10ef3f3e2
Muhammad Usman Nasir <usmannasir451@gmail.com>
2026-05-18 23:17:52 +0500

    added custom snipforce favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,9 +77,15 @@ export const metadata: Metadata = {
     creator: '@snipforce',
   },
   icons: {
-    icon: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
-    shortcut: '/favicon.svg',
-    apple: '/favicon.svg',
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
   },
   other: {
     'geo.region': 'PK',
